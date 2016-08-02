5f667b6bf8545794070635146b126b7528e175e7
Trey Frank Boehm <treyfrankboehm@gmail.com>
2016-08-02 12:42:58 -0500

    updated license (COPYING)

--- roastcalc.py
+++ roastcalc.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+### Disclaimer ### {{{
+# This file is part of roastcalc
+# 
+# roastcalc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more detail.
+# <http://www.gnu.org/licenses/>
+
+# You should have received a copy of the GNU General Public License
+# along with roastcalc.  If not, see <http://www.gnu.org/licenses/>.
+### }}}
+
 ### Magic numbers ### {{{
 # Add this percent to roast input weight
 safetyBuffer = 0.005
--- scrape.py
+++ scrape.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
-# Most of this code was taken from quickstart.py on the official Google
+### Disclaimer ### {{{
+# This file is part of roastcalc
+# 
+# roastcalc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more detail.
+# <http://www.gnu.org/licenses/>
+
+# You should have received a copy of the GNU General Public License
+# along with roastcalc.  If not, see <http://www.gnu.org/licenses/>.
+### }}}
+
+# Most of this code is based on quickstart.py from the official Google
 # page for the Sheets API.
 # The values of interest are spreadsheetId, sheetNames, and
 # credential_path. Change these to suit the needs of the program.
